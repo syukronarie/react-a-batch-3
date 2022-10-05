80bf8d7a40b09fe940d4261a5629fa0d1f566e8b
anangkf_ <gonanggoneng@gmail.com>
2022-10-05 22:25:51 +0700

    added router provider

--- react-fe-dummyio/src/index.js
+++ react-fe-dummyio/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import WebRoutes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WebRoutes>
+      <App />
+    </WebRoutes>
   </React.StrictMode>
 );
 
