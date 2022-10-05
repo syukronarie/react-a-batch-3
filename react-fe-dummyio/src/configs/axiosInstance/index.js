e95c5f8bbd2340f12efe3e84ccceaa5b52cab0a8
Abdullah Nouval Shidqi <87747795+AbdullahNouvalShidqi@users.noreply.github.com>
2022-10-05 20:45:04 +0700

    feat/teamA: axiosInstance, API, utils for const added

--- react-fe-dummyio/src/configs/axiosInstance/index.js
+++ react-fe-dummyio/src/configs/axiosInstance/index.js
@@ -3,6 +3,9 @@ import CONST from "../../utils/contants";
 
 const config = {
 	baseURL: CONST.BASE_URL_API,
+	headers: {
+		"app-id": CONST.REACT_APP_KEY_ID,
+	}
 };
 
 const axiosInstance = axios.create(config);
