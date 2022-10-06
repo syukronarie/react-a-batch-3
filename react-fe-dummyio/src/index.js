cbd2f41e42ea991c585cf90258556aa17ad346e1
anangkf_ <gonanggoneng@gmail.com>
2022-10-06 22:00:07 +0700

    added sidebar

--- react-fe-dummyio/src/index.js
+++ react-fe-dummyio/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './App.css';
 import reportWebVitals from './reportWebVitals';
 import WebRoutes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WebRoutes>
-      <App />
-    </WebRoutes>
+    <WebRoutes /> 
   </React.StrictMode>
 );
 
--- react-fe-dummyio/src/routes/index.js
+++ react-fe-dummyio/src/routes/index.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "../App";
+import Layout from "../components/Layout";
 import UpdateUser from "../pages/UpdateUser";
 
 const WebRoutes = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" >
-					<Route path='/edit-user' element={<UpdateUser />}/>
+				<Route path="/" element={<Layout />}>
+					<Route path='/user/edit' element={<UpdateUser />}/>
 				</Route>
 				<Route path="*" />
 			</Routes>
