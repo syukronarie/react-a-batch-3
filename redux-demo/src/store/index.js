80f2bc0ceab15d09f8012ea2ab004f93c68e5f0a
syukronarie <syukronarie@gmail.com>
2022-10-10 19:11:58 +0700

    team-b

--- redux-demo/src/store/index.js
+++ redux-demo/src/store/index.js
@@ -1,10 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
+
+import {
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+
+import persistStore from "redux-persist/lib/persistStore";
+import persistReducer from "redux-persist/lib/persistReducer";
+import storage from "redux-persist/lib/storage";
 import rootReducer from "./features";
 
+const persistConfig = {
+	key: "root",
+	whitelist: ["cart"],
+	blacklist: ["loader", "products", "user"],
+	storage,
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-	reducer: rootReducer,
-	middleware: [thunk],
+	reducer: pReducer,
+	middleware: (getDefaultMiddleware) => {
+		return getDefaultMiddleware({
+			serializableCheck: {
+				ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		});
+	},
 });
 
+const persistor = persistStore(store);
+
+export { persistor };
 export default store;
